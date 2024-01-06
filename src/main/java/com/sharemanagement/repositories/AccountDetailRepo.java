b9b87857e18c40ba4485f84e93c07ccc07a855e0
pahashiinfotech <pahashiinfotech@gmail.com>
2024-01-06 23:59:03 +0530

    dine changes in pom

--- src/main/java/com/sharemanagement/repositories/AccountDetailRepo.java
+++ src/main/java/com/sharemanagement/repositories/AccountDetailRepo.java
@@ -1,7 +1,6 @@
 package com.sharemanagement.repositories;
 
 import com.sharemanagement.entities.AccountDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
